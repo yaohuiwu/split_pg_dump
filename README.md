941dff7cebe57b884c162ec903d9175af1781c93
AJ Greyling <agreyling@mezzanineware.com>
2018-06-01 15:19:37 +0200

    Added more parameters for optional omision of type and sequence

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # split_pg_dump
 Split / parse pg_dump output to individual files per object
 
+<pre>
+usage: split_pg_dump.py [-h] [-ns] [-nt] sourcefile
+
+Split output from pg_dump into seperate files. See
+https://github.com/ajgreyling/split_pg_dump
+
+positional arguments:
+  sourcefile        SQL input file. Typically from pg_dump
+
+optional arguments:
+  -h, --help        show this help message and exit
+  -ns, -nosequence  Omit the sequence number prefix for resulting filenames.
+                    Handy for comparing schemas between databases
+  -nt, -notype      Ommit the type prefix in resulting filenames
+</pre>
+
 Tested on PostrgeSQL 9.6 and Ubuntu 14.04.2 LTS
 
 The python script <a href="https://github.com/ajgreyling/split_pg_dump/blob/master/split_pg_dump.py">split_pg_dump.py</a> will take the output of pg_dump above as parameter and generate multiple .sql files in the current directory. Each filename is prefixed with a sequence number so that they can be run in the correct order (for depandancies)
 
 To make the resulting files portable (to allow you applying the changes to another database or schema) perform pg_dump with the following parameters: -s -x -O
 
+<b>Output Filenames<b>
 
-Output Files (from split_pg_dump python script)
+Default s with no options
 <pre>
 00001_ty_some_type.sql
 00002_fn_some_function.sql
@@ -18,5 +35,24 @@ Output Files (from split_pg_dump python script)
 00006_ix_some_index.sql
 </pre>
 
+-ns , -nosequene
+<pre>
+ty_some_type.sql
+fn_some_function.sql
+vw_some_view.sql
+mv_some_materialised_view.sql
+sq_some_sequence.sql
+ix_some_index.sql
+</pre>
+
+-nt , -notype
+<pre>
+00001_some_type.sql
+00002_some_function.sql
+00003_some_view.sql
+00004_some_materialised_view.sql
+00005_some_sequence.sql
+00006_some_index.sql
+</pre
 
 <a href="https://github.com/ajgreyling/split_pg_dump/blob/master/bulkpsql.sh">bulkpsql.sh</a> will allow you to apply the whole folder of files to the server by only specifying your password and schema once.
--- split_pg_dump.py
+++ split_pg_dump.py
@@ -4,20 +4,22 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='Split output from pg_dump into seperate files. See https://github.com/ajgreyling/split_pg_dump')
-parser.add_argument('sourcefile', help='SQL input file. Typically from pg_dump')
+parser.add_argument('sourcefile',help='SQL input file. Typically from pg_dump')
+parser.add_argument('-ns','-nosequence', dest='nosequence',action='store_true',help='Omit the sequence number prefix for resulting filenames. Handy for comparing schemas between databases')
+parser.add_argument('-nt','-notype', dest='notype',action='store_true',help='Ommit the type prefix in resulting filenames' )
 args = parser.parse_args()
 
 type_prefix = {
-    'MATERIALIZED VIEW' : '_mv_',
-    'SEQUENCE' : '_sq_',
-    'INDEX' : '_ix_',
-    'TABLE' : '_tb_',
-    'TYPE' : '_ty_',
-    'VIEW' : '_vw_',
-    'FUNCTION' : '_fn_'
+    'MATERIALIZED VIEW' : 'mv_',
+    'SEQUENCE' : 'sq_',
+    'INDEX' : 'ix_',
+    'TABLE' : 'tb_',
+    'TYPE' : 'ty_',
+    'VIEW' : 'vw_',
+    'FUNCTION' : 'fn_'
 }
-
-print "input file:", args.sourcefile
+#print "input file:", args.sourcefile
+print args.nosequence
 inputfile = ''
 inputfile=args.sourcefile
 # delete contents of 00000_pre_execute.sql. If it does not exist, create it
@@ -30,19 +32,23 @@ with open(inputfile) as fo:
         filename = ''
         object_type_set = set()
         for line in fo.readlines():
-            match_result = re.search('-- Name: (?P<object_name>\w+\(?\)?); Type: (?P<object_type>\w+ ?\w+);', line)
+            match_result = re.search('-- Name: (?P<object_name>\w+)\(?\)?; Type: (?P<object_type>\w+ ?\w+);', line)
             if not (match_result is None):
                 newfile = True
                 object_name = match_result.group('object_name')
                 object_type = match_result.group('object_type')
-                if object_name == 'FUNCTION':
-                    print object_name
                 skip = (object_type == 'SCHEMA' or object_type == 'TABLE' or '__idx_he_' in object_name or '__he_' in object_name or object_name in '__change_')
                 if not skip :
                     object_type_set.add(object_type)
 
                     if (newfile):
-                        filename = '{0:05d}'.format(cntr) + type_prefix[object_type] + object_name + '.sql'
+                        filename = ''
+                        if not args.nosequence:
+                            filename = '{0:05d}'.format(cntr) + '_'
+                        if not args.notype:
+                            filename += type_prefix[object_type]
+                        filename += object_name + '.sql'
+
                         print filename
                         with open (filename,'w') as opf:
                             if object_type == 'VIEW' or object_type == 'MATERIALIZED VIEW':
