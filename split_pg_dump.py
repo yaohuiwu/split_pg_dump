9391428aeb9d06be2eb95751a5ee20e89a0be8b9
AJ Greyling <agreyling@mezzanineware.com>
2018-06-01 16:31:36 +0200

    removed hard-coding from MVP and added command line arguments to make it generic and re-usable

--- split_pg_dump.py
+++ split_pg_dump.py
@@ -5,9 +5,24 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Split output from pg_dump into seperate files. See https://github.com/ajgreyling/split_pg_dump')
 parser.add_argument('sourcefile',help='SQL input file. Typically from pg_dump')
+parser.add_argument('-of','-outputdir',dest='outputdir',help='Optional alternative destination / output directory for SQL output files')
 parser.add_argument('-ns','-nosequence', dest='nosequence',action='store_true',help='Omit the sequence number prefix for resulting filenames. Handy for comparing schemas between databases')
 parser.add_argument('-nt','-notype', dest='notype',action='store_true',help='Ommit the type prefix in resulting filenames' )
+parser.add_argument('-nc','-noclean', dest='noclean',action='store_true',help='Skip the step of cleaning the target directory of *.sql' )
+parser.add_argument('-xn','-exludenames', dest='excludenames',nargs='+', help='Exclude objects these strings in their names')
+parser.add_argument('-xt','-exludetypes', dest='exludetypes',nargs='+', help='Exclude objects these types')
 args = parser.parse_args()
+
+def should_be_skipped(object_name,object_type):
+    for exclude_name in args.excludenames:
+        if exclude_name in object_name:
+            print "skipping due to name: " + object_name + " type: " + object_type
+            return True
+    for exclude_type in args.exludetypes:
+        if exclude_type == object_type:
+            print "skipping due to type: " + object_type + " name: " + object_name
+            return True
+    return False
 
 type_prefix = {
     'MATERIALIZED VIEW' : 'mv_',
@@ -16,14 +31,26 @@ type_prefix = {
     'TABLE' : 'tb_',
     'TYPE' : 'ty_',
     'VIEW' : 'vw_',
-    'FUNCTION' : 'fn_'
+    'FUNCTION' : 'fn_',
+    'SCHEMA' : 'sc_'
 }
-#print "input file:", args.sourcefile
-print args.nosequence
+
+
 inputfile = ''
 inputfile=args.sourcefile
-# delete contents of 00000_pre_execute.sql. If it does not exist, create it
-#open('00000_pre_execute.sql', 'w').close()
+
+output_dir = ''
+if args.outputdir is None:
+    output_dir = os.path.dirname(os.path.realpath(__file__))
+else:
+    output_dir = args.outputdir
+
+if not args.noclean:
+    files = os.listdir(output_dir)
+    for filename in files:
+        if filename.endswith(".sql"):
+            os.remove(os.path.join(output_dir,filename))
+
 
 with open(inputfile) as fo:
         skip = True
@@ -37,7 +64,8 @@ with open(inputfile) as fo:
                 newfile = True
                 object_name = match_result.group('object_name')
                 object_type = match_result.group('object_type')
-                skip = (object_type == 'SCHEMA' or object_type == 'TABLE' or '__idx_he_' in object_name or '__he_' in object_name or object_name in '__change_')
+#                skip = object_type == 'SCHEMA' or object_type == 'TABLE' or should_be_skipped(object_name)
+                skip = should_be_skipped(object_name,object_type)
                 if not skip :
                     object_type_set.add(object_type)
 
@@ -48,6 +76,7 @@ with open(inputfile) as fo:
                         if not args.notype:
                             filename += type_prefix[object_type]
                         filename += object_name + '.sql'
+                        filename = os.path.join(output_dir,filename)
 
                         print filename
                         with open (filename,'w') as opf:
@@ -68,8 +97,5 @@ with open(inputfile) as fo:
                         opf.close
 fo.close()
 
-#def write_pre_excute(object_name)
-#    with open ('00000_pre_execute.sql','a') as opf:
-#        opf.write('\nDO $$ BEGIN\n')
-#        opf.write('PERFORM __he_delete_table_or_view__(\'' + object_name + '\');\n')
-#        opf.write('END $$;\n\n')   
+
+    
